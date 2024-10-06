35af578391774e4daceef07e534de9ef6779f2e2
adzaky <muhadlim17@gmail.com>
2024-10-06 12:30:12 +0700

    feat: add sidebar scroll effect transition color and reduce white color opacity

--- src/components/common/sidebar/sidebar-overlay.tsx
+++ src/components/common/sidebar/sidebar-overlay.tsx
@@ -5,7 +5,7 @@ type SidebarOverlayProps = {
 const SidebarOverlay = ({ setShowSidebar }: SidebarOverlayProps) => {
   return (
     <div
-      className="fixed top-0 bottom-0 left-0 right-0 z-30 flex md:hidden bg-black/40"
+      className="fixed top-0 bottom-0 left-0 right-0 z-30 flex md:hidden"
       onClick={() => {
         setShowSidebar(false);
       }}
