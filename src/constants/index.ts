c49a34abec39ce6784ae54146f64309c76a7d781
vickyadri29 <vickyadri290501@gmail.com>
2024-11-11 12:14:54 +0700

    chore: update speaker datas

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,14 +21,14 @@ export const navLists: NavList[] = [
 export const speakerLists: SpeakerList[] = [
   {
     id: 1,
-    name: 'Adiatma Kamarudin',
-    role: 'Principal Engineer at eFishery',
-    img: '/assets/images/speaker11.png'
+    name: 'Rian Apriansyah',
+    role: 'Software Engineer at Unit4',
+    img: '/assets/images/rian.png'
   },
   {
     id: 2,
     name: 'Fendi Irfan Amorokhman',
     role: 'Data Engineer at OY Indonesia',
-    img: '/assets/images/speaker22.png'
+    img: '/assets/images/fandi.png'
   }
 ]
--- src/features/home/components/speakers/card.tsx
+++ src/features/home/components/speakers/card.tsx
@@ -5,8 +5,8 @@ import React, { FC } from "react";
 const Card: FC<{ data: SpeakerList }> = ({ data }) => {
   return (
     <div>
-      <div className="w-full space-y-4 p-2">
-        <Image src={data.img} alt={data.name} width={240} height={240} className="rounded-full" />
+      <div className="w-full space-y-4 flex flex-col items-center p-2">
+        <Image src={data.img} alt={data.name} width={240} height={240} className="w-40 h-40 object-cover rounded-full" />
         <div className="flex flex-col items-center gap-1">
           <p className="text-white md:text-xl font-semibold">{data.name}</p>
           <p className="text-white text-sm">{data.role}</p>
